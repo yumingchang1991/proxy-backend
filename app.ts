bd562d077760dde505347f1626a380a70196ffe9
ChillDiver <yumingchang1991@gmail.com>
2022-08-06 23:04:24 +0800

    fix port assignment

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 const app: express.Application = express()
 
-const PORT: number = 3000
+const PORT: number = Number(process.env.PORT) || 3000
 const BASE_URL: string = 'http://api.marketstack.com/v1/'
 
 // Marketstack API: https://marketstack.com/documentation
@@ -14,7 +14,7 @@ const BASE_URL: string = 'http://api.marketstack.com/v1/'
 
 // /api/:symbol/
 app.get('/', (req, res) => {
-  res.send('it is working!')
+  res.json('it is working!')
 })
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
