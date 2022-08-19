ab4f2ab4cafa2c00fc6967e32f85d2e31af10d61
ChillDiver <yumingchang1991@gmail.com>
2022-08-19 17:03:22 +0800

    add api to route and add apiLimiter

--- app.ts
+++ app.ts
@@ -3,16 +3,27 @@ if (process.env.NODE_ENV !== 'production') {
 }
 import express from 'express'
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
 import router from './routes/index'
 
 const app: express.Application = express()
-
 const PORT: number = Number(process.env.PORT) || 8080
+
+app.set('trust proxy', 1)
+
 app.use(cors({ origin: process.env.CLIENT_ORIGIN }))
 app.use(express.urlencoded({ extended: true }))
 // Marketstack API: https://marketstack.com/documentation
 // End-of-day Data & Historical Data
 
-app.use(router)
+app.get('/ip', (req, res) => {
+  res.send(req.ip)
+})
+
+const apiLimiter = rateLimit({
+  windowMs: 20 * 60 * 1000,
+  max: 30
+})
+app.use('/api', apiLimiter, router)
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
