2ea6d9b735327a1e7309f02c17c2d732e66b8980
ChillDiver <yumingchang1991@gmail.com>
2022-08-20 22:45:41 +0800

    feat: add apicache

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express'
-
+import apicache from 'apicache'
 import eodRouter from './modules/eod'
 
 const router:Router = express.Router()
 
-router.use('/eod/latest', eodRouter)
+router.use('/eod/latest', apicache.middleware('60 minutes'), eodRouter)
 router.use('/', (req, res, next) => {
   res.json('this is root path!')
 })
