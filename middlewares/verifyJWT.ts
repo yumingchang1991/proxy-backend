39a22e25e99b5a0b1ca930a8e06f143e2f4137cd
ChillDiver <yumingchang1991@gmail.com>
2022-10-04 22:47:11 +0800

    doc: remove console.log

--- middlewares/verifyJWT.ts
+++ middlewares/verifyJWT.ts
@@ -9,13 +9,10 @@ interface iUserPayload {
 }
 
 const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-  console.log('this request is going through verifyJWT')
   try {
     const authHeader = req.headers.authorization
-    console.log('authHeader: ', authHeader)
     if (!authHeader) return res.json({ status: 'error', message: 'no valid token'})
     const token = authHeader.split(' ')[1]
-    console.log('token: ', token)
     // this line returns payload object with signature if no error
     // if there is error, try/catch will catch them
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string)
@@ -23,9 +20,7 @@ const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
     // if no error, then we cast decodedToken as our interface
     const decodedToken = jwt.decode(token) as iUserPayload
     const { username } = decodedToken
-
     req.user = username
-    console.log('verifyJWT, req.user: ', req.user)
     next()
   } catch (err) {
     return res.json({ status: 'error', message: 'token is extracted but considered manipulated' })
