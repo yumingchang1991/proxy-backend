e3f3e7e90f2d0fbc471486e3846643da1e325e21
ChillDiver <yumingchang1991@gmail.com>
2022-11-10 10:58:36 +0800

    ref: rethrow error

--- services/symbolService.ts
+++ services/symbolService.ts
@@ -1,14 +1,12 @@
-import { Response } from 'express'
 import Symbol from "../models/symbol"
-import errorHandler from "../middlewares/errorHandlers"
 
 const symbolService = {
-  async getSymbol (res: Response, input: string) {
+  async getSymbol (input: string) {
     try {
       const symbol = await Symbol.findOne({ symbol: input })
       return symbol
     } catch (err) {
-      errorHandler.general(res, `${input} is not a valid symbol in this application`)
+      throw Error(`${input} is not a valid symbol in this application`)
     }
   }
 }
